012d71ffc4e31072ae38bda91d4089a1167a083b
NahNathan <nathanrodriguessantos3@gmail.com>
2024-04-19 10:51:57 -0300

    Mais detalhes

--- Blog-Angular/src/app/components/menu-title/menu-title.component.ts
+++ Blog-Angular/src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: ['./menu-title.component.css', './menu-title.responsive.component.css']
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit, OnDestroy {
+  private letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private interval: any;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.randomizeLetters(); // Chamada da função ao inicializar o componente
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  randomizeLetters(): void {
+    const target = document.querySelector('h1[data-value="Bem Vindo!"]') as HTMLElement; // Seleciona o elemento diretamente
+    const originalText = target.getAttribute('data-value')!;
+    let iteration = 0;
+
+    clearInterval(this.interval);
+
+    this.interval = setInterval(() => {
+      target.innerText = originalText
+        .split('')
+        .map((letter, index) => {
+          if (index < iteration) {
+            return originalText[index];
+          }
+          return this.letters[Math.floor(Math.random() * 26)];
+        })
+        .join('');
+
+      iteration += 1 / 3;
+
+      if (iteration >= originalText.length) {
+        clearInterval(this.interval);
+        target.innerText = originalText; // Restaura o texto original no fim
+      }
+    }, 50);
+  }
 }
--- Blog-Angular/src/app/data/dataFake.ts
+++ Blog-Angular/src/app/data/dataFake.ts
@@ -3,56 +3,56 @@ export const dataFake=[
         "Id":"1",
         "photo":"https://wallpapercave.com/wp/wp7250087.jpg",
         "title":"Projeto de Gerenciamento de Consultórios Médicos",
-        "description":"Este sistema foi feito para a matéria de Programação Orientada Pa Objetos, do curso de ADS que estou cursando no IFTM. Este projeto foi feito em dupla e durante todo o semestre conforme aprendiamos sobre os conceitos de POO como abstração, encapsulamento, polimorfismo e herança. Foi desenvolvido completamente em Java.",
+        "description":"Este sistema foi feito para a matéria de Programação Orientada Pa Objetos, do curso de Análise e Desenvolvimento de Sistemas, no IFTM. O software oferece funcionalidades completas de CRUD (Criar, Ler, Atualizar, Deletar) para gerenciar perfis de usuários, médicos, franquias, unidades e consultas médicas. Com um design orientado para diferentes tipos de usuários, como gestores de franquia, médicos e pacientes, o sistema possibilita o gerenciamento eficiente desde o cadastro de pessoal até o controle financeiro de cada unidade, incluindo pagamentos e recebimentos. Ideal para garantir operações suaves e transparência na gestão de serviços médicos em escala. É possível . Este projeto foi feito em dupla e durante todo o semestre conforme aprendiamos sobre os conceitos de POO como abstração, encapsulamento, polimorfismo e herança. Foi desenvolvido completamente em Java.",
         "link": "https://github.com/NahNathan/iftm-consultorio-medico-2023-01"
     },
     {
         "Id":"2",
         "photo":"https://raw.githubusercontent.com/NahNathan/Emprestimo-de-Chaves/main/Doc/README_assets/insert_pg.png",
         "title":"Sistema de Empréstimo de Chaves",
-        "description":"Projeto para a disciplina de Desenvolvimento de Aplicações Web",
+        "description":"Este projeto foi desenvolvido para a disciplina Desenvolvimento de Aplicações Web 1 e consiste em um programa web completo para o gerenciamento de empréstimos de chaves. Utilizamos SpringBoot (Java) e MongoDB para o back-end, e Svelte para o front-end, criando uma interface limpa e funcional. O software registra todas as atividades de empréstimo e devolução de chaves, permitindo um controle eficiente e seguro. Através deste projeto, aprimoramos nossas habilidades em integração de API, arquitetura de banco de dados e UI/UX, além de expandir nosso conhecimento em bancos de dados NoSQL. Foi feito em dupla com luiz-felippelb",
         "link": "https://github.com/NahNathan/Emprestimo-de-Chaves"
     },
     {
         "Id":"3",
         "photo":"https://i.imgur.com/7GLZ3wC.png",
         "title":"Jeff Imóveis, um sistema kotlin de gerenciamento de imóveis feito para uma prova",
-        "description":"Projeto feito em dupla para a disciplina de Programação de Dispositivos Móveis.",
+        "description":"O projeto Jeff-Imóveis foi desenvolvido como parte da avaliação final da disciplina de Programação para Dispositivos Móveis do IFTM, em colaboração com luiz-felippelb. Este sistema permite o cadastro de imóveis, proprietários e inquilinos, oferecendo funcionalidades para visualização e edição desses dados. Além disso, possibilita o aluguel de imóveis aos inquilinos cadastrados, facilitando a gestão de propriedades de forma eficiente e integrada diretamente de dispositivos móveis.",
         "link": "https://github.com/NahNathan/Jeff-Imoveis"
     },
     {
         "Id":"4",
         "photo":"https://i.imgur.com/802ZQgU.png",
         "title":"Pokédex",
-        "description":"Um projeto clássico para vários desenvolvedores. Este projeto foi desenvolvido em Angular e seu principal objetivo foi aprender a como utilizar e consumir APIs externas.",
+        "description":"Um projeto clássico para vários desenvolvedores. Este projeto foi desenvolvido em Angular e seu principal objetivo foi aprender a como utilizar e consumir APIs externas. O projeto consome a PokémonAPI, e mostra dados do pokémon, assim como possibilitar pesquisar por um pokémon específico.",
         "link": "https://github.com/NahNathan/Pokedex"
     },
     {
         "Id":"5",
         "photo":"https://i.imgur.com/5eWzXu3.png",
         "title":"Campo Minado Mobile em Kotlin",
-        "description":"Um joguinho legal em Kotlin.",
+        "description":"Esse projeto foi desenvolvido para a matéria de Extensão. Consiste em um jogo simples de campo minado feito usando Kotlin e as telas foram feitas usando xml. Ao final da matéria o projeto foi apresentado para uma turma de alunos do ensino fundamental para que criassem um interesse pela área de programação e pelo IFTM. Foi feito em dupla com Augusto-Castejon",
         "link": "https://github.com/NahNathan/Campo-Minado-Mobile"
     },
     {
         "Id":"6",
         "photo":"https://i.imgur.com/xg7WO8X.png",
         "title":"Night City Tarot",
-        "description":"Apenas.",
+        "description":"O Night City Tarot é um aplicativo web que eu desenvolvido para usar em meus jogos pessoais de Cyberpunk Red, um RPG de mesa. Ele utiliza um baralho de cartas de tarô com tema Cyberpunk para apresentar eventos e resultados críticos aleatórios durante cenários de combate. O aplicativo aciona efeitos únicos das cartas da Arcana Maior quando certas condições de combate ocorem, como rolar vários seis. Sinte-se livre para dar uma olhada, ou até adicionar algo a mais.",
         "link": "https://nahnathan.github.io/Night_City_Tarot/"
     },
     {
         "Id":"7",
         "photo":"https://i.imgur.com/QAvFKfI.png",
         "title":"NatCursos",
-        "description":"Um projeto simples que fiz para estudar aspectos e conceitos do framework Svelte.",
+        "description":"Um projeto simples que fiz para estudar aspectos e conceitos do framework Svelte. Sendo apenas o front para um 'mock-up', o projeto é um site que apresenta vários cursos e projetos disponíveis, login e cadastro, favoritar cursos e também cursos concluídos. O objetivo deste projeto é apenas para aprendizado e treinar minhas habilidades com a tecnologia e desenvolvimento.",
         "link": "https://nat-cursos.vercel.app/"
     },
     {
         "Id":"8",
         "photo":"https://i.imgur.com/B8sTUFS.png",
         "title":"Imagens Responsivas",
-        "description":"Um projeto simples para estudar vários conceitos de javascrpit e um efeito visual muito interessante.",
+        "description":"Um projeto simples para estudar vários conceitos de javascrpit e um efeito visual muito interessante. Nesse projeto várias imagens são mostradas à medida que o cursor se move pela tela",
         "link": " https://github.com/NahNathan/Imagens_Responsivas"
     },
     {
