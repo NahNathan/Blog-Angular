36913de7761987aa3ff055da79ad19d5f7756760
NahNathan <nathanrodriguessantos3@gmail.com>
2024-10-31 17:45:11 -0300

    Update Text

--- Blog-Angular/src/app/components/menu-title/menu-title.component.ts
+++ Blog-Angular/src/app/components/menu-title/menu-title.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.css', './menu-title.responsive.component.css']
+  styleUrls: ['./menu-title.component.css']
 })
 export class MenuTitleComponent implements OnInit, OnDestroy {
   private letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
